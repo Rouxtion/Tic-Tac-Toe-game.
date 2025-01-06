9b65e0d1cccbf9d7f023f2b13adafcd99faa5924
abdulsamet ozmelioglu <121859420+Rouxtion@users.noreply.github.com>
2025-01-06 22:26:47 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,16 +1,48 @@
-﻿using System;
+using System;
 
 class TicTacToe
 {
-    static char[] board = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
-    static char currentPlayer = 'X';
+    static char[] board;
+    static char currentPlayer;
 
     static void Main(string[] args)
     {
-        Console.WriteLine("Welcome to Tic-Tac-Toe!");
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("1. New game");
+            Console.WriteLine("2. About the author");
+            Console.WriteLine("3. Exit");
+            Console.Write("> ");
+            string menuChoice = Console.ReadLine();
+
+            switch (menuChoice)
+            {
+                case "1":
+                    StartNewGame();
+                    break;
+                case "2":
+                    ShowAbout();
+                    break;
+                case "3":
+                    if (ConfirmExit())
+                        return;
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
+        }
+    }
+
+    static void StartNewGame()
+    {
+        board = new char[] { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+        currentPlayer = 'X';
 
         while (true)
         {
+            Console.Clear();
             PrintBoard();
             Console.WriteLine($"{currentPlayer}'s move (1-9) > ");
             string input = Console.ReadLine();
@@ -18,12 +50,27 @@ class TicTacToe
             if (int.TryParse(input, out int move) && move >= 1 && move <= 9 && board[move - 1] == ' ')
             {
                 board[move - 1] = currentPlayer;
-                if (IsBoardFull())
+
+                if (CheckWin())
                 {
+                    Console.Clear();
                     PrintBoard();
-                    Console.WriteLine("Game over!");
+                    Console.WriteLine($"{currentPlayer} won!");
+                    Console.WriteLine("[Press Enter to return to main menu...]");
+                    Console.ReadLine();
                     break;
                 }
+
+                if (IsBoardFull())
+                {
+                    Console.Clear();
+                    PrintBoard();
+                    Console.WriteLine("It's a draw!");
+                    Console.WriteLine("[Press Enter to return to main menu...]");
+                    Console.ReadLine();
+                    break;
+                }
+
                 currentPlayer = currentPlayer == 'X' ? 'O' : 'X';
             }
             else
@@ -50,5 +97,43 @@ class TicTacToe
                 return false;
         }
         return true;
+    }
+
+    static bool CheckWin()
+    {
+        int[,] winningCombinations = new int[,]
+        {
+            { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 }, // Rows
+            { 0, 3, 6 }, { 1, 4, 7 }, { 2, 5, 8 }, // Columns
+            { 0, 4, 8 }, { 2, 4, 6 }              // Diagonals
+        };
+
+        for (int i = 0; i < winningCombinations.GetLength(0); i++)
+        {
+            if (board[winningCombinations[i, 0]] == currentPlayer &&
+                board[winningCombinations[i, 1]] == currentPlayer &&
+                board[winningCombinations[i, 2]] == currentPlayer)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static void ShowAbout()
+    {
+        Console.Clear();
+        Console.WriteLine("Tic-Tac-Toe Game");
+        Console.WriteLine("Author: Your Name");
+        Console.WriteLine("This game is a simple implementation of Tic-Tac-Toe.");
+        Console.WriteLine("Press any key to return to the menu...");
+        Console.ReadKey();
+    }
+
+    static bool ConfirmExit()
+    {
+        Console.Write("Are you sure you want to quit? [y/n] > ");
+        string confirmation = Console.ReadLine()?.ToLower();
+        return confirmation == "y";
     }
 }
